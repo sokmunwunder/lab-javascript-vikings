ab7ddbb9ef6b7abb5935856edced8dcaa37d01d4
Sok Mun Wunder <sokmunwunder@gmail.com>
2020-11-14 12:43:45 +0100

    Iteration 1 -2 (without battle cry)

--- src/viking.js
+++ src/viking.js
@@ -1,11 +1,36 @@
 // Soldier
-class Soldier {}
+class Soldier {
+    constructor(health, strength){
+        this.health=health;
+        this.strength=strength;
+    }
+    attack(){
+        return this.strength;
+    }
+
+    receiveDamage(theDamage){
+        return this.health -= theDamage;}
+    }
+
+
 
 // Viking
-class Viking {}
+class Viking extends Soldier {
+    constructor(name, health, strength){
+        super(health, strength);
+        this.name=name;
+    }
+
+    receiveDamage(theDamage){
+        let newHealth = this.health -=theDamage
+        if(newHealth > 0){
+            return `${this.name} has received ${theDamage} points of damage`}
+       else {return `${this.name} has died in act of combat`};
+    }
+}
 
 // Saxon
-class Saxon {}
+//class Saxon {}
 
 // War
-class War {}
+// class War {}
