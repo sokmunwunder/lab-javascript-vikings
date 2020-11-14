783e2138d403399bc937d64a7c1563e080d829f4
Sok Mun Wunder <sokmunwunder@gmail.com>
2020-11-14 12:57:58 +0100

    Iteration 2 completed

--- src/viking.js
+++ src/viking.js
@@ -22,15 +22,31 @@ class Viking extends Soldier {
     }
 
     receiveDamage(theDamage){
-        let newHealth = this.health -=theDamage
+        let newHealth = this.health -=theDamage;
         if(newHealth > 0){
             return `${this.name} has received ${theDamage} points of damage`}
        else {return `${this.name} has died in act of combat`};
     }
+
+    battleCry(){
+        return "Odin Owns You All!";
+    }
 }
 
 // Saxon
-//class Saxon {}
+class Saxon extends Soldier {
+    constructor(health, strength){
+        super(health, strength);
+    }
+
+    receiveDamage(theDamage){
+        let newHealthSaxon=this.health-=theDamage;
+        if (newHealthSaxon >0){
+            return `A Saxon has received ${theDamage} points of damage`}
+        else {return "A Saxon has died in combat"};
+        }
+    }
+
 
 // War
 // class War {}
